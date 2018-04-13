65ddadae8468547683a06a51b82c1a29fd899819
cavinhuang <943866180@qq.com>
2018-04-13 22:27:59 +0800

    完成touch start 记录

--- src/components/swiper/index.vue
+++ src/components/swiper/index.vue
@@ -18,19 +18,53 @@ export default {
   name: 'swiper',
   data () {
     return {
-      items: []
+      items: [], // itmes
+      touchData: {} // 移动数据
     }
   },
   mounted () {
     this.initItems()
+
+    // 绑定时事件
+    const $el = this.$el
+
+    $el.addEventListener('touchstart', this.touchStart, false)
+    $el.addEventListener('touchmove', this.touchMove, false)
+    $el.addEventListener('touchEnd', this.touchEnd, false)
   },
   methods: {
+    /**
+     * 初始化Item
+     * @return {[type]} [description]
+     */
     initItems () {
       const $el = this.$refs.swiperUl.children
       for (var i = 0; i < $el.length; i++) {
         this.items.push($el[i])
       }
-    }
+    },
+    /**
+     * touch start event handle
+     * @param  {[Object]} e [touch event]
+     * @return {[type]}   [description]
+     */
+    touchStart (e) {
+      const touchs = e.touchs[0] ? e.touchs[0] : e
+      this.touchData.startTime = Date.now() // 开始时的时间戳
+      this.touchData.startX = touchs.pageX // 开始时的横坐标
+    },
+    /**
+     * touch move event handle
+     * @param  {[Object]} e [touch event]
+     * @return {[type]}   [description]
+     */
+    touchMove (e) {},
+    /**
+     * touch end event hendle
+     * @param  {[Object]} e [touch event]
+     * @return {[type]}   [description]
+     */
+    touchEnd (e) {}
   }
 }
 </script>
