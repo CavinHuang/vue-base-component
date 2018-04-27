9adb446395c0927c05a448b16308d46a824f72b1
cavinhuang <943866180@qq.com>
2018-04-27 22:33:42 +0800

    初步完成message

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ import '@/assets/css/iconfont.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue( {
-	el: '#app',
-	router,
-	components: { App },
-	template: '<App/>'
-} )
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
